4308004a8f55047f34841a92accd22a8b5e4dd31
Marcos Porto <mosther_marcos007@hotmail.com>
2023-06-30 19:19:06 -0300

    alteration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
+  eslint: { ignoreDuringBuilds: true },
 }
 
 module.exports = nextConfig
